ac5574ecea8eb30d695597aaa83cfe0108e9064a
Karthick Prassana <prassana001001@gmail.com>
2025-01-23 20:10:48 +0530

    Update bug resolved.

--- app/src/main/java/com/example/todo/pages/MainViewPage.kt
+++ app/src/main/java/com/example/todo/pages/MainViewPage.kt
@@ -1,6 +1,19 @@
 package com.example.todo.pages
 
 import AppViewModel
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
@@ -31,8 +44,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -42,6 +60,10 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.todo.R
 import com.example.todo.data.ToDo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainPageView(navHostController: NavHostController, viewModel: AppViewModel) {
@@ -52,23 +74,26 @@ fun MainPageView(navHostController: NavHostController, viewModel: AppViewModel) 
         modifier = Modifier.fillMaxSize(),
         containerColor = colorResource(R.color.page),
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    navHostController.navigate("${Screens.AddUpdate.route}/0")
-                },
-                elevation = FloatingActionButtonDefaults.elevation(10.dp),
-                containerColor = colorResource(R.color.button),
-                shape = RoundedCornerShape(30.dp),
-                modifier = Modifier
-                    .size(60.dp)
-                    .padding()
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.Add,
-                    contentDescription = "Add a todo",
-                    tint = colorResource(R.color.page),
-                    modifier = Modifier.padding(10.dp)
+            AnimatedVisibility(
+                visible = true,
+                enter = scaleIn(
+                    animationSpec = tween(100)
+                ) + fadeIn(
+                    animationSpec = tween(200)
+                ),
+                exit = scaleOut(
+                    animationSpec = tween(100)
+                ) + fadeOut(
+                    animationSpec = tween(200)
                 )
+            ) {
+                FloatingActionButton(
+                    onClick = { navHostController.navigate("${Screens.AddUpdate.route}/0") },
+                    containerColor = colorResource(R.color.button),
+                    shape = RoundedCornerShape(30.dp)
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = "Add")
+                }
             }
         }
     ) {
@@ -100,27 +125,52 @@ fun ItemViewer(
             val state = rememberSwipeToDismissBoxState(
                 confirmValueChange = {
                     if (it == SwipeToDismissBoxValue.EndToStart) {
-                        viewModel.deleteToDo(todo)
+                        CoroutineScope(Dispatchers.Main).launch {
+                            delay(300)
+                            viewModel.deleteToDo(todo)
+                        }
                         true
                     } else {
                         false
                     }
                 }
             )
-
+            val visible by remember { mutableStateOf(true) }
             Spacer(modifier = Modifier.height(10.dp))
-            SwipeToDismissBox(
-                state = state,
-                backgroundContent = {},
-                enableDismissFromEndToStart = true,
-                enableDismissFromStartToEnd = false,
-                content = {
-                    ItemDesign(todo, viewModel) {
-                        val id = todo.id
-                        navController.navigate("${Screens.AddUpdate.route}/${id}")
+
+            AnimatedVisibility(
+                visible = visible,
+                enter = slideInVertically(
+                    initialOffsetY = {
+                        it
+                    },
+                    animationSpec = tween(500)
+                ) + fadeIn(
+                    animationSpec = tween(600)
+                ),
+                exit = slideOutVertically(
+                    targetOffsetY = {it},
+                    animationSpec = tween(500)
+                ) + fadeOut(
+                    animationSpec = tween(600)
+                ),
+
+
+                ) {
+                SwipeToDismissBox(
+                    state = state,
+                    backgroundContent = {},
+                    enableDismissFromEndToStart = true,
+                    enableDismissFromStartToEnd = false,
+                    content = {
+                        ItemDesign(todo, viewModel) {
+                            val id = todo.id
+                            navController.navigate("${Screens.AddUpdate.route}/${id}")
+                        }
                     }
-                }
-            )
+                )
+            }
+
         }
     }
 }
@@ -132,7 +182,9 @@ fun ItemDesign(todo: ToDo, viewModel: AppViewModel, onClick: () -> Unit) {
     Card(
         shape = RoundedCornerShape(30.dp),
         colors = CardDefaults.cardColors(colorResource(R.color.card)),
-        modifier = Modifier.fillMaxWidth(0.9f).clickable { onClick() },
+        modifier = Modifier
+            .fillMaxWidth(0.9f)
+            .clickable { onClick() },
 
         ) {
         Row(
